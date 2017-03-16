75b2da80eca673203a41ffacc2beb97860a1e2a3
Sanderson, Deuvarney <deuvarney@yahoo.com>
2017-03-16 11:14:42 -0400

    Add ability for using custom parsers

--- src/parsers/index.js
+++ src/parsers/index.js
@@ -1,16 +1,39 @@
 import cucumber from './cucumber'
 import cucumberMulti from './cucumber.multi'
+import { extname } from 'path'
 import multi from './multi'
 import standard from './standard'
 
 let all = { cucumber, cucumberMulti, multi, standard }
 
-function getParser (name) {
-  if (name && all[name]) {
-    return all[name]
-  } else {
-    throw new Error(`Invalid Parser Specified: ${name}`)
+function handlePath (parserPath) {
+  try {
+    parserPath = `${process.cwd()}/${parserPath}`;
+    let customParserPath = require.resolve(parserPath)
+    return require(customParserPath)
+  } catch (e) {
+    throw new Error(`Invalid Custom Parser Path Specified: ${parserPath}`)
   }
+}
+
+function handleFlakeParser (parserName) {
+  if (all[parserName]) {
+    return all[parserName]
+  } else {
+    throw new Error(`Invalid Parser Specified: ${parserName}`)
+  }
+}
+
+function getParser (parser = '') {
+  // Not exactly sure what this is for yet
+  // if (parser.hasOwnProperty('parse')) {
+  //   return handleObject(parser)
+  // }
+  if (extname(parser)) {
+    return handlePath(parser)
+  }
+
+  return handleFlakeParser(parser)
 }
 
 export default { all, getParser }
